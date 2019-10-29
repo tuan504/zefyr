c921eca8ec46d0727b395a52ded86f9ad121396e
Cedric GESTES <cedric.gestes@gmail.com>
2019-10-29 12:02:59 +0100

    linting: fix unused _scrollController

--- packages/zefyr/lib/src/widgets/editable_text.dart
+++ packages/zefyr/lib/src/widgets/editable_text.dart
@@ -222,6 +222,7 @@ class _ZefyrEditableTextState extends State<ZefyrEditableText>
   // Private members
   //
 
+  // ignore: unused_field
   final ScrollController _scrollController = ScrollController();
   ZefyrRenderContext _renderContext;
   CursorTimer _cursorTimer;
